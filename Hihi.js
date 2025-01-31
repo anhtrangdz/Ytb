928279b5cc02dba814669c2ec57e7517cf2a10f6
anhtrangdz <166999301+anhtrangdz@users.noreply.github.com>
2025-01-31 15:05:55 +0700

    Update Hihi.js

--- Hihi.js
+++ Hihi.js
@@ -1,50 +1,68 @@
-// YouTube Premium Unlock Script for Shadowrocket
-// Created by: anhtrangdz
-
 const config = {
-    hideDownloadButton: true,
-    hideClipButton: true,
-    subtitleLanguage: "vi",
-    lyricsLanguage: "vi",
-    scriptEngine: "jsc",
     enableDebug: false,
+    scriptEngine: "jsc",
 };
 
 const url = $request.url;
-const method = $request.method;
 
 if (url.includes("youtubei.googleapis.com/youtubei/v1/player")) {
     let response = JSON.parse($response.body);
-    
-    // Bật tính năng phát nền
-    if (response.hasOwnProperty("playbackTracking")) {
-        response.playbackTracking = {};
+
+    // Chặn tất cả quảng cáo (trước, giữa, sau, tua, pop-up)
+    response.adPlacements = [];
+    response.playerAds = [];
+    response.midroll = [];
+    response.overlay = [];
+    response.endScreen = [];
+    response.paidContentOverlay = [];
+
+    // Chặn các request quảng cáo khác nếu có
+    if (response.hasOwnProperty("adBreakParams")) {
+        delete response.adBreakParams;
     }
-    
-    // Chặn quảng cáo
-    if (response.hasOwnProperty("adPlacements")) {
-        response.adPlacements = [];
+    if (response.hasOwnProperty("adSignals")) {
+        delete response.adSignals;
     }
-    
-    // Chặn midroll ads (quảng cáo giữa video)
-    if (response.hasOwnProperty("playerAds")) {
-        response.playerAds = [];
+    if (response.hasOwnProperty("adSurvey")) {
+        delete response.adSurvey;
     }
-    
-    // Tối ưu phụ đề và lời bài hát
-    if (config.subtitleLanguage !== "off") {
-        response.captions = {
-            language: config.subtitleLanguage
-        };
+
+    // Tối ưu chất lượng âm thanh
+    response.streamingData?.formats?.forEach(format => {
+        format.audioQuality = "high";  // Cài đặt âm thanh chất lượng cao
+    });
+
+    // Xóa thông tin quảng cáo mà YouTube có thể sử dụng để hiển thị quảng cáo
+    delete response.adServingData;
+    delete response.playerConfig?.adBreakConfig;
+
+    // Giữ lại videoDetails nhưng xóa thông tin quảng cáo để tránh bị phát hiện
+    if (response.hasOwnProperty("videoDetails")) {
+        delete response.videoDetails.isLive;
+        delete response.videoDetails.adPlacements;
     }
-    if (config.lyricsLanguage !== "off") {
-        response.lyrics = {
-            language: config.lyricsLanguage
-        };
-    }
-    
-    // Gửi phản hồi
+
     $done({ body: JSON.stringify(response) });
-} else {
+}
+
+// Giả mạo tài khoản Premium để thử bật PiP và phát nhạc nền
+else if (url.includes("youtubei.googleapis.com/youtubei/v1/account/get")) {
+    let response = JSON.parse($response.body);
+    if (response.account?.membership) {
+        response.account.membership.level = "Premium";
+    }
+    $done({ body: JSON.stringify(response) });
+}
+
+// Giả mạo Premium để thử mở khóa PiP và Background Play
+else if (url.includes("youtubei.googleapis.com/youtubei/v1/player")) {
+    let response = JSON.parse($response.body);
+    if (response.hasOwnProperty("playbackTracking")) {
+        response.playbackTracking.premium = true;
+    }
+    $done({ body: JSON.stringify(response) });
+} 
+
+else {
     $done({});
 }
