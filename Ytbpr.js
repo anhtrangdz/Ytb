59a4e144502227b626116dc39e402c61b8587796
anhtrangdz <166999301+anhtrangdz@users.noreply.github.com>
2025-02-07 22:17:13 +0700

    Update Ytbpr.js

--- Ytbpr.js
+++ Ytbpr.js
@@ -1,11 +1,18 @@
 let body = JSON.parse($response.body);
 
-// Xóa quảng cáo (siêu gọn, siêu hiệu quả)
+// Xóa quảng cáo trên trang chính & trong video
 if (body.adPlacements) body.adPlacements = [];
+if (body.playerAds) body.playerAds = [];
+if (body.responseContext?.mainAppWebResponseContext) {
+  body.responseContext.mainAppWebResponseContext.adSlots = [];
+}
 
 // Đổi tiêu đề thành "YouTube Premium"
 if (body.header?.c4TabbedHeaderRenderer) {
   body.header.c4TabbedHeaderRenderer.title = "YouTube Premium";
+}
+if (body.metadata?.channelMetadataRenderer) {
+  body.metadata.channelMetadataRenderer.title = "YouTube Premium";
 }
 
 $done({ body: JSON.stringify(body) });
